60af14f6320676d192ee2a4d39b9feb963e84b20
silvia <clarinet221@gmail.com>
2021-11-21 17:37:20 +0800

    add image avif format in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    // loader: 'imgix',
-    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // path: '/_next/static/media/',
+    formats: ['image/avif', 'image/webp'],
   },
 }
